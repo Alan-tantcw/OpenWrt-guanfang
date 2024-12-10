1544295d998f9f1f3bddc946adec6f5a2c1fcf35
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-12-10 15:54:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
+# 替换 feeds.conf.default 中的 packages 源
+sed -i 's|^src-git packages .*|src-git packages https://github.com/kenzok8/small-package.git|' feeds.conf.default
 
-echo 'src-git packages https://github.com/kenzok8/small-package.git' >>feeds.conf.default
